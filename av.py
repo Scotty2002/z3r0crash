176880344af1a195a7b11695711e34a84919d636
Scotty2002 <scottyarts@yahoo.com>
2017-01-18 04:09:48 -0600

    Update and rename av6.py to av.py

--- av.py
+++ av.py
@@ -9,7 +9,7 @@ for path, subdirs, files in os.walk(root):
     for name in files:
        if fnmatch(name, pattern):
              os.path.join(path, name)
-file1 = open("C:\Users\joebl\Desktop\Programming\Virus Soruce Codes\Virus Research\VirusSignaturesDB.txt", "r")
+file1 = open("C:\Users\username\Desktop\VirusSignaturesDB.txt", "r")
 file2 = open(os.path.join(path, name), "r") 
 list1 = file1.readlines()
 list2 = file2.readlines()
